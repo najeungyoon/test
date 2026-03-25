9f580211b40ffbc81a2c47fa183d7bb5ab6f996e
najeungyoon <najy1177@gmail.com>
2026-03-25 15:25:49 +0900

    slave02 last fix

--- test03.py
+++ test03.py
@@ -3,5 +3,6 @@ def sayHello(name):
     return 'Hello, ' + name + "i"
     
 inName = 'Tom'
-print(sayHello(inName))
+lastName = 'jeungyoon'
+print(sayHello(inName + lastName))
 print("main 브랜치에서 slave02가 새로운 커밋 생성")
